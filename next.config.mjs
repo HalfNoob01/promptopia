8bee3576228d3bbd63342cd1b4dffcce0552660a
HalfNoob01 <MAHATOSANJAY207@GMAIL.COM>
2024-05-28 16:38:55 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-        ],
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+export default nextConfig
